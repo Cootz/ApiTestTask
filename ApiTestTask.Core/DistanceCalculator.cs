4cf1755447f05c5dfa8623dbe07e69b9de776c92
Cootz <artem2004799@gmail.com>
2023-06-07 14:14:59 +0300

    Add distance calculation

---
+++ ApiTestTask.Core/DistanceCalculator.cs
@@ -0,0 +1,55 @@
+﻿using ApiTestTask.Core.Helpers;
+using static ApiTestTask.Core.Helpers.MathHelper;
+
+namespace ApiTestTask.Core
+{
+    public class DistanceCalculator
+    {
+        /// <summary>
+        /// Earth radius in meters
+        /// </summary>
+        public const double EARTH_RADIUS = 6_371_000;
+
+        /// <summary>
+        /// Calculates sum of distances between given <paramref name="coordinates"/>
+        /// </summary>
+        /// <returns>Sum of distances between given <paramref name="coordinates"/>. If number of <paramref name="coordinates"/> &lt; 2 returns 0 </returns>
+        public double Calculate(IEnumerable<Coordinate>? coordinates)
+        {
+            double distanceSum = 0;
+
+            if (coordinates is null)
+                return distanceSum;
+
+            Coordinate[] coordinatesArray = coordinates.ToArray();
+
+            if (coordinatesArray.Length < 2)
+                return distanceSum;
+
+            for (int i = 1; i < coordinatesArray.Length; i++)
+            {
+                distanceSum += calculateDistance(coordinatesArray[i - 1], coordinatesArray[i]);
+            }
+
+            return distanceSum;
+        }
+
+        private double calculateDistance(Coordinate c1, Coordinate c2)
+        {
+            (double sinC1, double cosC1) = GetSinCosFor(c1.Longitude.ToRadians());
+            (double sinC2, double cosC2) = GetSinCosFor(c2.Longitude.ToRadians());
+
+            double deltaLatitude = c2.Latitude - c1.Latitude;
+            (double sinDelta, double cosDelta) = GetSinCosFor(deltaLatitude);
+
+            //See reference formula here - https://wiki.gis-lab.info/images/8/89/Great-cirlcles-09.gif
+            double top =
+                Math.Sqrt(Math.Pow(cosC2 * sinDelta, 2) + Math.Pow(cosC1 * sinC2 - sinC1 * cosC2 * cosDelta, 2));
+
+            double bottom = sinC1 * sinC2 + cosC1 * cosC2 * cosDelta;
+
+            double distance = Math.Atan2(top, bottom) * EARTH_RADIUS;
+            return distance;
+        }
+    }
+}
---
+++ ApiTestTask.Core/Helpers/MathHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiTestTask.Core.Helpers
+{
+    public static class MathHelper
+    {
+        public static double ToRadians(this double value) => value * Math.PI / 180;
+
+        public static (double, double) GetSinCosFor(double value)
+        {
+            double cos = Math.Cos(value);
+            double sin = Math.Sin(value);
+
+            return (sin, cos);
+        }
+    }
+}
